f2d2030a1c22f3bffaa7b02260610557acb9ae1f
Brendan McCollam <brendan@mccoll.am>
2023-08-13 16:18:51 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "title.pest"]
+pub struct OurParser;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let unparsed_file = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    let file = OurParser::parse(Rule::file, &unparsed_file);
+        //.expect("unsuccessful parse"); // unwrap the parse result
+        //.next().unwrap(); // get and unwrap the `file` rule; never fails
+    println!("{:?}", file);
+}
