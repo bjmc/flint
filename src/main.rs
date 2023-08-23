52fd4e4369b266bcd5c4c2e4a5d360e7da56f2b8
Brendan McCollam <brendan@mccoll.am>
2023-08-23 21:53:59 +0100

    Attempt testing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,27 @@ use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
-#[grammar = "fountain.pest"]
+#[grammar = "hackery.pest"]
 pub struct OurParser;
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+  //use mycrate::parser::{OurParser, Rule};
+  use lazy_static::lazy_static;
+  use pest_test::{Error, PestTester};
+
+  lazy_static! {
+    static ref TESTER: PestTester<Rule, OurParser> =
+      PestTester::from_defaults(Rule::root_rule);
+  }
+
+  // Loads test from `tests/pest/mytest.txt` and evaluates it. Returns an `Err<pest_test::Error>`
+  // if there was an error evaluating the test, or if the expected and actual values do not match.
+  fn test_my_parser() -> Result<(), Error> {
+    (*TESTER).evaluate_strict("mytest")
+  }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
